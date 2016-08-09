2f646ad3adbc12bec1b0a9d138b302dd1655cdc1
Ralf W. Grosse-Kunstleve <rwgk@terrabella.com>
2016-08-09 00:59:48 +0000

    new ppjson.py preserving key order

---
+++ bin/ppjson.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+import collections
+import json
+import sys
+
+
+def run(args):
+  for filename in args:
+    data = json.load(open(filename), object_pairs_hook=collections.OrderedDict)
+    print json.dumps(data, indent=4)
+
+
+if __name__ == '__main__':
+  run(args=sys.argv[1:])
