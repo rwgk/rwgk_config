d920214eb0aa22cd1e7fd2480e410ef5bc84243d
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2017-05-02 17:30:47 -0700

    New strip_c_comments.py

---
+++ bin/strip_c_comments.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+import subprocess
+import sys
+
+
+def run(args):
+  for filename in args:
+    divider = ':' * len(filename)
+    if len(args) > 1:
+      print divider
+      print filename
+      print divider
+    # http://stackoverflow.com/questions/2394017/remove-comments-from-c-c-code
+    cmd = ['gcc', '-fpreprocessed', '-dD', '-E', filename]
+    lines = subprocess.check_output(cmd, shell=False).splitlines()
+    prev_line = 'BOF'
+    for line in lines:
+      line = line.rstrip()
+      if line.startswith('#'):
+        line = ''
+      if line or prev_line:
+        print line
+      prev_line = line
+
+
+if __name__ == '__main__':
+  run(args=sys.argv[1:])
