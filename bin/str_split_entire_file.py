b8d83cd1c35b829595fa3020d615a3efb55bd800
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2017-05-26 16:37:55 -0700

    New str_split_entire_file.py.

---
+++ bin/str_split_entire_file.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import sys
+
+
+def read_split_print(file_like_obj):
+  for line in file_like_obj.read().split():
+    print line
+
+
+def run(args):
+  if args:
+    for filename in args:
+      read_split_print(open(filename))
+  else:
+    read_split_print(sys.stdin)
+
+
+if (__name__ == '__main__'):
+  run(args=sys.argv[1:])
