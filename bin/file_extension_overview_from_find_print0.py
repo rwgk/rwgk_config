06be0c96969a3f07fbf2821521c440af0c85289f
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2017-11-16 17:08:23 -0800

    new file_extension_overview command

---
+++ bin/file_extension_overview_from_find_print0.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import collections
+import os
+import sys
+
+
+def run(args):
+  assert not args
+  ext_counts = collections.defaultdict(int)
+  for line in sys.stdin.read().split('\0'):
+    unused_root, ext = os.path.splitext(line)
+    ext_counts[ext] += 1
+  for count, ext in reversed(sorted([(count, ext) for ext, count in ext_counts.items()])):
+    if not ext:
+      ext = None
+    print '%8d %s' % (count, ext)
+
+
+if __name__ == '__main__':
+  run(args=sys.argv[1:])
