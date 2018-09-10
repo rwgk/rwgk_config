06ced1c96f99839714e68dc937b44477f96f9c2f
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2018-09-10 12:38:36 -0700

    minimal simple new join_lines.py

---
+++ bin/join_lines.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from __future__ import print_function
+
+import sys
+
+
+def Run(args):
+  assert len(args) == 1, 'num_lines'
+  num_lines = int(args[0])
+  lines = sys.stdin.read().splitlines()
+  for i in xrange(0, len(lines), num_lines):
+    print(' '.join(lines[i:i + num_lines]))
+
+
+if __name__ == '__main__':
+  Run(args=sys.argv[1:])
