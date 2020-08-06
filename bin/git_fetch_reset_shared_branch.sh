c53363c02ee5dc2ca1d57e61400bbc9682bed62c
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2020-08-06 14:39:06 -0700

    adding --hard options

--- bin/git_fetch_reset_shared_branch.sh
+++ bin/git_fetch_reset_shared_branch.sh
@@ -4,4 +4,4 @@ SHARED_BRANCH_NAME="$1"
 git show-ref --verify --quiet refs/heads/"$SHARED_BRANCH_NAME"
 git fetch upstream
 git checkout "$SHARED_BRANCH_NAME"
-git reset upstream/"$SHARED_BRANCH_NAME"
+git reset --hard upstream/"$SHARED_BRANCH_NAME"
