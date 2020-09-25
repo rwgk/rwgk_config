d815d637d2ec8ce1d71927984abf7c614766b58e
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2020-09-25 12:16:47 -0700

    removing commands no longer needed

--- bin/git_fetch_reset_shared_branch.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-set -e -x
-SHARED_BRANCH_NAME="$1"
-git show-ref --verify --quiet refs/heads/"$SHARED_BRANCH_NAME"
-git fetch upstream
-git checkout "$SHARED_BRANCH_NAME"
-git reset --hard upstream/"$SHARED_BRANCH_NAME"
