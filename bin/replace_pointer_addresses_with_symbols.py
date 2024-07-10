2cfee2b8e53e190ff544108dd4f5e419d1ba7dd6
Ralf W. Grosse-Kunstleve <rwgk@google.com>
2024-07-10 06:50:26 -0700

    New replace_pointer_addresses_with_symbols.py

---
+++ bin/replace_pointer_addresses_with_symbols.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+
+r"""Replace all PTR=123456= with PTR=AAA= in order seen.
+
+Example C++ code:
+    printf("\nPTR=%ld=\n", (long) ptr);
+"""
+
+import re
+import string
+import sys
+
+
+def run(args):
+  assert len(args) == 1, "filename"
+  blob = open(args[0]).read()
+  mappings = {}
+  for ptrnum in re.findall(r"PTR=\d+=", blob):
+    ptrsym = mappings.get(ptrnum)
+    if ptrsym is None:
+      ptrsym = "PTR=%s=" % (string.ascii_uppercase[len(mappings)] * 3)
+      mappings[ptrnum] = ptrsym
+      blob = blob.replace(ptrnum, ptrsym)
+  print(blob, end="")
+
+
+if __name__ == "__main__":
+  run(args=sys.argv[1:])
