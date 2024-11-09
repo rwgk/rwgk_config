0f5929c66c7af89ab91d56f618831fa1eed67c2a
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2024-11-09 13:48:00 -0800

    vscode_dotfiles_install.sh experiment: .inputrc, .vimrc, $HOME/.vim

--- vscode_dotfiles_install.sh
+++ vscode_dotfiles_install.sh
@@ -1,1 +1,7 @@
-echo "LOOOK vscode_dotfiles_install.sh"
+#! /bin/bash
+cd "$HOME"
+ln -s dotfiles/inputrc .inputrc
+ln -s dotfiles/vimrc .vimrc
+mkdir -p "$HOME/.vim/backup"
+mkdir -p "$HOME/.vim/swap"
+mkdir -p "$HOME/.vim/undo"
