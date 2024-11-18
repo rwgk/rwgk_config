8cef65d1908b0ce268bf9b08f5d3930a123b5927
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2024-11-18 15:16:33 -0800

    Modernize save_or_move.sh: insert timestamp before extension (similar to mtimestamp.sh)

--- bin/save_or_move.sh
+++ bin/save_or_move.sh
@@ -2,35 +2,42 @@
 
 set -e
 
-if [ -z "$Save" ]; then
-  echo "$0"': FATAL: missing $Save: please define.'
+if [ -z "$RWGK_CONFIG_SAVE_TO" ]; then
+  echo "$0"': FATAL: missing $RWGK_CONFIG_SAVE_TO: please define.'
   exit 1
 fi
 
 mode="$1"
 shift
-if [ "$mode" != sve -a "$mode" != smv ]; then
-  echo "$0"': FATAL: unknown mode:' "$mode"
+if [ "${mode}" != sve -a "${mode}" != smv ]; then
+  echo "$0"': FATAL: unknown mode:' "${mode}"
   exit 1
 fi
 
 now=`date "+%Y-%m-%d+%H%M%S"`
 
 for arg in "$@"; do
-  if [ -L "$arg" ]; then
+  if [ -L "${arg}" ]; then
     cmd=cp
   else
-    if [ "$mode" == "sve" ]; then
+    if [ "${mode}" == "sve" ]; then
       cmd='cp -a'
     else
       cmd=mv
     fi
   fi
-  barg=$(basename "$arg")
-  echo $cmd "$arg" "$Save/${barg}_$now"
-  $cmd "$arg" "$Save/${barg}_$now"
-  if [ "$cmd" == cp -a "$mode" == smv ]; then
-    rm "$arg"
+  filename=$(basename "${arg}")
+  barename="${filename%.*}"
+  if [ X"${barename}" = X -o X"${barename}" = X"${filename}" ]; then
+    newname="${filename}_${now}"
+  else
+    extension="${filename##*.}"
+    newname="${barename}_${now}.${extension}"
+  fi
+  echo ${cmd} "${arg}" "$RWGK_CONFIG_SAVE_TO/${newname}"
+  ${cmd} "${arg}" "$RWGK_CONFIG_SAVE_TO/${newname}"
+  if [ "${cmd}" == cp -a "${mode}" == smv ]; then
+    rm "${arg}"
   fi
 done
 
