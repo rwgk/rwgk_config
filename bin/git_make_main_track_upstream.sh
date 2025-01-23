e8da1b669277410eaf6b5bafdf55a6154dcca2c4
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-01-23 13:20:13 -0800

    git_make_main_track_upstream.sh: show usage if number of arguments is not correct.

--- bin/git_make_main_track_upstream.sh
+++ bin/git_make_main_track_upstream.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+if [ $# -ne 2 ]; then
+  echo "usage: $0 upstream_url main_branch_name  # exactly two arguments required, $# given."
+  exit 1
+fi
+set -e
 set -x
 git remote add upstream "$1"
 git fetch upstream
