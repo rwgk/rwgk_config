0bcedead1297aa1a779c962054d08c9e52fa308c
Ralf W. Grosse-Kunstleve <rgrossekunst@nvidia.com>
2025-07-08 20:54:33 -0700

    Microsoft.PowerShell_profile.ps1: add $wintermsettings

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 # copy Microsoft.PowerShell_profile.ps1 "$PROFILE"
 $uh = "\\wsl$\Ubuntu\home\$env:USERNAME"
+$wintermsettings = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 $env:Path += ";C:\Program Files\Vim\vim91"
 function gitbash { & "C:\Program Files\Git\bin\bash.exe" -l }
 function mf3path { & "$env:USERPROFILE\AppData\Local\miniforge3\shell\condabin\conda-hook.ps1" }
